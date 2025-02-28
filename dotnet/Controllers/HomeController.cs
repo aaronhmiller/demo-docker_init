4b4ba6d986f8a4c57cb2283871a8f2515a5f8356
Aaron Miller <aaronmiller+gpg@gmail.com>
2025-02-27 23:39:11 -0800

    adding .NET

---
+++ dotnet/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+// Controllers/HomeController.cs
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using HelloWorld.Models;
+
+namespace HelloWorld.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            ViewData["Message"] = "Hello World from ASP.NET Core MVC!";
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
+
